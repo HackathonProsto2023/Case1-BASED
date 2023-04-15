65b02822c3b912cf74dadb90e2bf2b09c76a9b52
Yuriy <komrad.kim-j@yandex.ru>
2023-04-15 17:44:03 +0300

    feat: Add response for recruiter page

--- frontend/src/pages/profile-recruiter/ProfileRecruiterPage.tsx
+++ frontend/src/pages/profile-recruiter/ProfileRecruiterPage.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import "../profile-recruiter/ProfileRecruiterPageStyle.css"
 
 const ProfileRecruiterPage = () => {
+    const [responseCount, setResumesCount] = useState(0)
+    function onMessageClicked() {
+
+    }
     return (
         <div>
-            ProfileVacancyPage
+            <h1 style={{color: 'green'}}>Страница рекрутера</h1>
+            <div>
+                У вас {responseCount} непросмотренных резюме
+                <button>Просмотреть резюме</button>
+            </div>
         </div>
     );
 };
