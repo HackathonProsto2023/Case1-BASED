9493f23b1a8ddafc0d502b8b0f956ef2e8cb14b2
ValikSilchenko <valik.silchenko@gmail.com>
2023-04-15 13:55:46 +0300

    ci: add setup.py configuration

---
+++ src/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='recruitment_app',
+    version='1.0.0',
+    description='Приложение для удобного подбора персонала',
+    long_description="",
+    zip_safe=False,
+    packages=['app', 'tests'],
+    python_requires='>=3.10',
+)
