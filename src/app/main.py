47a8186ee55c754036362d005b379b3ee89fbb89
Glicheres <artem.sizov.2002@list.ru>
2023-04-15 23:46:41 +0300

    feat: add CORS

--- src/app/main.py
+++ src/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.app.responses.routers import response_router
 from src.app.users.routers import users_router
@@ -6,6 +7,22 @@ from src.app.vacancies.routers import vacancies_router
 
 app = FastAPI(
     title= "Hi Mark"
+)
+
+# front-end availability
+origins = [
+    "http://localhost:3000"
+    "http://localhost:8000"
+    "http://localhost"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTION", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                    "Authorization"],
 )
 
 app.include_router(response_router)
